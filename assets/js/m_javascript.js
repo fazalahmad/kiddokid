fedc31ca4e12c7437573d6d06ea5ceb719ba121f
operasional <operasional2017@outlook.com>
2018-07-23 12:01:57 +0700

    first website

---
+++ assets/js/m_javascript.js
@@ -0,0 +1,35 @@
+$('#cart-dropdown').on('click', function(){
+  $(this).toggleClass('open')
+})
+
+
+
+// $('.t-cart').hover(function(){
+//   $('.t-cart-add').show();
+//   // alert('lol')
+// })
+
+// $('.t-cart').on('mouseout', function(){
+//   $('.t-cart-add').css("display","none");
+// })
+
+// function open_cart(){
+//   if (!$('.cart').hasClass('open')) {
+//     $('.card').addClass('open')
+//   }
+//   else{
+//     $('.card').removeClass('open')
+//   }
+// }
+
+
+//
+// function close_cart(){
+//   $('#mycart').css("height","0");
+//   $('#mycart').css("width","0");
+//   $('#mycart').css("display","none");
+// }
+//
+// $('body').on('click', function(){
+//   close_cart()
+// })
---
+++ index.php
@@ -0,0 +1,205 @@
+<?php
+  include_once('header.php');
+ ?>
+<!--body-start-->
+<div class="container-fluid">
+  <h2>Video</h2>
+  <hr>
+  <div class="row" align="center">
+    <div class="col-6 col-md-4">
+      <a href="#" >
+        <div class="k-card t-cart" >
+          <img id="video" style="background-image:url(https://i.ytimg.com/vi/jXjVdIgIw8c/maxresdefault.jpg);" class="img-card">
+          <div class="k-card-body">
+            <h4 id="judul_video">Upin Ipin Terbaru 2017 | Animasi Terakhir Superboy Seru</h4>
+          </div>
+          <div class="k-card-footer">
+            <small id="publish_video" >Brad Traversy</small>
+            <strong id="harga_video" class="f-right">IDR 194.000,00</strong>
+          </div>
+
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+
+        </div>
+      </a>
+    </div>
+
+    <div class="col-6 col-md-4">
+      <a href="#" >
+        <div class="k-card t-cart">
+          <img style="background-image:url(https://i.ytimg.com/vi/jXjVdIgIw8c/maxresdefault.jpg);" class="img-card">
+          <div class="k-card-body">
+            <h4>Upin Ipin Terbaru 2017 | Animasi Terakhir Superboy Seru</h4>
+          </div>
+          <div class="k-card-footer">
+            <small>Brad Traversy</small>
+            <strong class="f-right">IDR 194.000,00</strong>
+          </div>
+
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+
+        </div>
+      </a>
+
+    </div>
+
+    <div class="col-6 col-md-4">
+      <a href="#">
+        <div class="k-card t-cart">
+          <img style="background-image:url(https://swebtoon-phinf.pstatic.net/20170803_157/1501762720374fIQbh_JPEG/thumbnail.jpg);" class="img-card">
+          <div class="k-card-body">
+            <h4>Upin Ipin Terbaru 2017 | Animasi Terakhir Superboy Seru</h4>
+          </div>
+          <div class="k-card-footer">
+            <small>Brad Traversy</small>
+            <strong class="f-right">IDR 250.000,00</strong>
+          </div>
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+  <center><button class="btn btn-info btn-lg" style="margin-top:2%;">More</button></center>
+</div>
+
+<div class="container-fluid">
+  <br><br>
+  <h2>Komik</h2>
+  <hr>
+  <div class="row" align="center">
+    <div class="col-6 col-md-4">
+      <a href="#">
+        <div class="k-card t-cart">
+        <!-- <img style="background-image:url(https://i.ytimg.com/vi/jXjVdIgIw8c/maxresdefault.jpg);" class="img-card"> -->
+        <img src="https://i.ytimg.com/vi/ebRL-f7tLIg/maxresdefault.jpg" alt="" class="img-card">
+          <div class="k-card-body">
+            <h4>Upin Ipin Terbaru 2017 | Animasi Terakhir Superboy Seru</h4>
+          </div>
+          <div class="k-card-footer">
+            <small>Brad Traversy</small>
+            <strong class="f-right">IDR 194.000,00</strong>
+          </div>
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-6 col-md-4">
+      <a href="#">
+        <div class="k-card t-cart">
+          <!-- <img style="background-image:url(https://i.ytimg.com/vi/jXjVdIgIw8c/maxresdefault.jpg);" class="img-card"> -->
+          <img src="https://meltingpotsandothercalamities.files.wordpress.com/2017/05/img_7554.jpg" alt="" class="img-card">
+          <div class="k-card-body">
+            <h4>Upin Ipin Terbaru 2017 | Animasi Terakhir Superboy Seru</h4>
+          </div>
+          <div class="k-card-footer">
+            <small>Brad Traversy</small>
+            <strong class="f-right">IDR 180.000,00</strong>
+          </div>
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-6 col-md-4">
+      <a href="#">
+        <div class="k-card t-cart">
+          <!-- <img style="background-image:url(https://i.ytimg.com/vi/jXjVdIgIw8c/maxresdefault.jpg);" class="img-card"> -->
+          <img src="https://swebtoon-phinf.pstatic.net/20170803_157/1501762720374fIQbh_JPEG/thumbnail.jpg" alt="" class="img-card">
+          <div class="k-card-body">
+            <h4>Upin Ipin Terbaru 2017 </h4>
+          </div>
+          <div class="k-card-footer">
+            <small>Brad Traversy</small>
+            <strong class="f-right">IDR 250.000,00</strong>
+          </div>
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+        </div>
+      </a>
+    </div>
+
+  </div>
+  <center><button class="btn btn-info btn-lg" style="margin-top:2%;">More</button></center>
+</div>
+
+<div class="container-fluid">
+  <br><br>
+  <h2>Majalah</h2>
+  <hr>
+  <div class="row" align="center">
+    <div class="col-6 col-md-4">
+      <a href="#">
+        <div class="k-card t-cart">
+        <img style="background-image:url(http://sales-jasatama.890m.com/wp-content/uploads/2016/09/10584-majalah-bobo-edisi-terbaru-15-terbit-21-juli-2016.jpg);" class="img-card">
+        <!-- <img src="http://sales-jasatama.890m.com/wp-content/uploads/2016/09/10584-majalah-bobo-edisi-terbaru-15-terbit-21-juli-2016.jpg" alt="" class="img-card"> -->
+          <div class="k-card-body">
+            <h4>Upin Ipin Terbaru 2017 | Animasi Terakhir Superboy Seru</h4>
+          </div>
+          <div class="k-card-footer">
+            <small>Brad Traversy</small>
+            <strong class="f-right">IDR 15.000,00</strong>
+          </div>
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-6 col-md-4">
+      <a href="#">
+        <div class="k-card t-cart">
+        <img style="background-image:url(https://scoopadm.apps-foundry.com/ebook-covers/39381/big_covers/ID_BOBO2017MTH07ED15_B.jpg);" class="img-card">
+        <!-- <img src="https://scoopadm.apps-foundry.com/ebook-covers/39381/big_covers/ID_BOBO2017MTH07ED15_B.jpg" alt="" class="img-card"> -->
+          <div class="k-card-body">
+            <h4>Upin Ipin Terbaru 2017 | Animasi Terakhir Superboy Seru</h4>
+          </div>
+          <div class="k-card-footer">
+            <small>Brad Traversy</small>
+            <strong class="f-right">IDR 20.000,00</strong>
+          </div>
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-6 col-md-4">
+      <a href="#">
+        <div class="k-card t-cart">
+        <img style="background-image:url(https://topbisnis.org/wp-content/uploads/2017/05/Pasang-Iklan-Majalah-Bobo-Junior.jpg);" class="img-card">
+        <!-- <img src="https://topbisnis.org/wp-content/uploads/2017/05/Pasang-Iklan-Majalah-Bobo-Junior.jpg" alt="" class="img-card"> -->
+          <div class="k-card-body">
+            <h4>Upin Ipin Terbaru 2017 | Animasi Terakhir Superboy Seru</h4>
+          </div>
+          <div class="k-card-footer">
+            <small>Brad Traversy</small>
+            <strong class="f-right">IDR 25.000,00</strong>
+          </div>
+          <div class="k-cart-add">
+            <button type="button" name="button" class="btn-add">Tambah ke Keranjang</button>
+          </div>
+        </div>
+      </a>
+    </div>
+
+  </div>
+    <center><button class="btn btn-info btn-lg" style="margin-top:2%;">More</button></center>
+</div>
+<!--body-end-->
+<?php
+  include_once('footer.php');
+ ?>
